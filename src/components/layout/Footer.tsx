c846c8ef8d7e2f258be43e8bb606693e689a68fa
sp <potikhanovsergey@gmail.com>
2023-02-26 21:24:39 +0300

    Footer updates

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,6 +8,7 @@ import {
   Tooltip,
 } from "@mantine/core";
 import ProductHuntExternalLink from "../ProductHuntExternalLink";
+import Image from "next/image";
 
 const Footer = () => {
   return (
@@ -18,10 +19,27 @@ const Footer = () => {
           <Group spacing={10}>
             <Text weight={500}>Made by</Text>
             <Tooltip label="Sergey Potikhanov" keepMounted>
-              <Anchor href="https://t.me/potikhanovsergey" target="_blank">
-                <Avatar src="https://sun1-19.userapi.com/s/v1/ig2/HcnhH6fPMj_eLSY4_TAbh2xLym2Qri-qRyn5KQmll5Om8RkhPvPLTm_HKH4f1xRr7SpVXM91LY6KiyvsA07MO4jD.jpg?size=400x400&quality=95&crop=529,5,1290,1290&ava=1" />
+              <Anchor
+                href="https://www.linkedin.com/in/sergey-potikhanov-647b87207/"
+                target="_blank"
+              >
+                <Avatar>
+                  <Image
+                    fill
+                    alt="Sergey Potikhanov"
+                    src="/images/potikhanov.jpeg"
+                  />
+                </Avatar>
               </Anchor>
             </Tooltip>
+            <Tooltip label="Darya Syomina" keepMounted>
+              <Anchor href="https://t.me/daryasyomina" target="_blank">
+                <Avatar>
+                  <Image fill alt="Darya Syomina" src="/images/syomina.jpg" />
+                </Avatar>{" "}
+              </Anchor>
+            </Tooltip>
+
             <Text>with Product Hunt API</Text>
           </Group>
         </Group>
