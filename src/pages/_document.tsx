b4644038c013cdc4e0c285f5029f3a3fed76bea2
sp <potikhanovsergey@gmail.com>
2023-03-04 11:44:21 +0300

    Add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,21 @@ export default class _Document extends Document {
       <Html>
         <Head>
           <link rel="icon" href="/productcell.svg" />
+          <meta
+            name="title"
+            content="See the best of Product Hunt through the life calendar"
+          />
+          <meta
+            name="description"
+            content="See the best of Product Hunt through a life calendar. Click on a cell to see the best products of the day. Filter the results by your favorite topics."
+          />
+          <meta
+            name="keywords"
+            content="Product Hunt, Life calendar, Calendar"
+          />
+          <meta name="robots" content="index, follow" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="English" />
         </Head>
         <body>
           <Main />
